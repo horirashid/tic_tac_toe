3d852f2a068348d1d2e55ab824a30825ae79e9c7
horirashid <hawry.jawad@gmail.com>
2021-06-30 11:17:30 +0900

    adding put and get functions

--- tictactoe.go
+++ tictactoe.go
@@ -1,2 +1,28 @@
 package main
 
+import "fmt"
+
+type Board struct {
+	tokens []int // tokens[0] -> (0,0), tokens[1] -> (0,1), ...
+}
+
+func (b *Board) put(x, y int, u string) {
+	if b.tokens[x + 3*y] == 0 {
+		if u == "o" {
+			b.tokens[x+3*y] = 1
+		} else if u == "x" {
+			b.tokens[x+3*y] = 2
+		}
+	} else {
+		fmt.Println("This cell is occupied!")
+	}
+}
+
+func (b *Board) get(x, y int) string {
+	if b.tokens[x + 3*y] == 1 {
+		return "o"
+	} else if b.tokens[x + 3*y] == 2 {
+		return "x"
+	}
+	return "."
+}
